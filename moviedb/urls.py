89b3add89c717f98bd0b141657e50454b581c510
James M. Allen <james.m.allen@gmail.com>
2015-10-08 11:05:58 -0400

    Top Movies view

--- moviedb/urls.py
+++ moviedb/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'movie/(?P<movie_id>\d+)$', views.movie_detail),
+    url(r'movie/(?P<movie_id>\d+)$', views.movie_detail, name='movie_detail'),
+    url(r'rater/(?P<rater_id>\d+)$', views.rater_detail, name='rater_detail'),
+    url(r'$', views.top_movies, name='top_movies'),
 ]
--- moviedb/views.py
+++ moviedb/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Movie
+from django.db.models import Avg, Count
+
+from .models import Movie, Rater
+
+
 # Create your views here.
 
 
@@ -8,3 +12,34 @@ def movie_detail(request, movie_id):
     return render(request,
                   'moviedb/movie_detail.html',
                   {'movie': movie})
+
+
+def rater_detail(request, rater_id):
+    rater = Rater.objects.get(pk=rater_id)
+    movie_ratings = []
+    for rating in rater.rating_set.all():
+        movie_ratings.append({
+            'movie': rating.movie,
+            'stars': '\u2605' * rating.stars,
+        })
+    return render(request,
+                  'moviedb/rater_detail.html',
+                  {'rater': rater,
+                   'movie_ratings': movie_ratings})
+
+
+def top_movies(request):
+    # movie_list = []
+    # for movie in Movie.objects.all():
+    #     if type(movie.average_rating()) == float:
+    #         movie_list.append(movie)
+
+    popular_movies = Movie.objects.annotate(num_ratings=Count('rating')) \
+                                  .filter(num_ratings__gte=50)
+
+    movies = popular_movies.annotate(Avg('rating__stars')) \
+                           .order_by('-rating__stars__avg')[:20]
+
+    return render(request,
+                  'moviedb/top_movies.html',
+                  {'movies': movies})
